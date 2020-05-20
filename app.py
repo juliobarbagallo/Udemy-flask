6750976585539f82f80542cc53ecf0df041dfe49
Julio Barbagallo <julio.barbagallo@gmail.com>
2020-05-20 16:06:02 -0300

    added debug=True on calling app.run, in order to avoid stop the server for each change.

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ wsgi_app = app.wsgi_app
 @app.route('/')
 def hello():
     """Renders a sample page."""
-    return "Hello World!!!"
+    return "Hello World!!! Yes!"
 
 if __name__ == '__main__':
     import os
@@ -22,4 +22,4 @@ if __name__ == '__main__':
         PORT = int(os.environ.get('SERVER_PORT', '5555'))
     except ValueError:
         PORT = 5555
-    app.run(HOST, PORT)
+    app.run(HOST, PORT, debug=True)
