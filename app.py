aea2d7a1d5d2fa7eb91a10a3c277a39c3f12a1fe
Julio Barbagallo <julio.barbagallo@gmail.com>
2020-05-20 16:35:46 -0300

    Added url_fpr and image on new static folder.

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ This script runs the application using a development server.
 It contains the definition of routes and views for the application.
 """
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 # Make the WSGI interface available at the top level so wfastcgi can get it.
@@ -13,7 +13,11 @@ wsgi_app = app.wsgi_app
 @app.route('/')
 def hello():
     """Renders a sample page."""
-    return "Hello World!!! Yes!"
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     import os
